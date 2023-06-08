9c32c6139b96fae409995e78144fa96151511929
Bryan Tavares <bryantavares25@gmail.com>
2023-06-07 22:56:00 -0300

    Updates

--- Proteases_systems_biology/bryan_test.R
+++ Proteases_systems_biology/bryan_test.R
@@ -38,7 +38,9 @@ data <- matrix(rnorm(100, 0, 10), nrow = 10, ncol = 10) # Create example data
 colnames(data) <- paste0("Col", 1:10) # Column names
 rownames(data) <- paste0("Row", 1:10) # Row names
 
-# # # # # MyTry # # # # # PresenceAbsencePlot
+# # # # # MyTry # # # # # HeatmapPlot
+
+#link video: https://www.youtube.com/watch?v=xerW2TvZHbQ
 
 name_col <-
 name_row <- 
@@ -47,3 +49,12 @@ test <- matrix(c(c(0,0,0,1,1,0,0,1,0,1,0,0), c(0,0,0,1,1,0,0,1,0,1,0,0), c(0,0,0
 mycolor <- colorRampPalette(c('green', 'darkgreen'))
 heatmap(test, col = mycolor(100))
 test
+
+# # # # # OthersTry # # # # PresenceAbsencePlot
+
+# Link: https://www.youtube.com/watch?v=zsY-dFuIWYs
+# Link: https://stats.stackexchange.com/questions/489508/best-way-to-visualise-presence-absence-of-specific-events-in-multiple-case-contr
+# Link: https://stackoverflow.com/questions/51641988/ggplot2-plotting-the-data-in-geom-tile-for-absence-or-presence
+# Link: https://stackoverflow.com/questions/65558154/r-dplyr-add-increasing-number-to-columns
+# Link: https://stackoverflow.com/questions/58222681/ploting-bianary-presence-absence-data-with-ggplot2-geom-point
+# Link: https://stackoverflow.com/questions/62876384/how-to-plot-binary-data-and-colour-presence-by-group-in-r
