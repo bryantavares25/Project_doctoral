81d932196a8248db9f405c5c3d4e5b9c9ef8decd
Bryan Tavares <bryantavares25@gmail.com>
2023-06-08 10:52:17 -0300

    New_figures

--- Proteases_sequence_retrieval/Poster_plot_08_06_23.R
+++ Proteases_sequence_retrieval/Poster_plot_08_06_23.R
@@ -1,6 +1,6 @@
 #### Aprendendo R ####
 
-# # # # # # 
+library(dplyr)
 
 ##### Example data
 set.seed(123) # Set seed for reproducibility
@@ -12,14 +12,44 @@ rownames(data) <- paste0("Row", 1:10) # Row names
 
 #link video: https://www.youtube.com/watch?v=xerW2TvZHbQ
 
-name_col <-
-name_row <- 
-  
-test <- matrix(c(c(0,0,0,1,1,0,0,1,0,1,0,0), c(0,0,0,1,1,0,0,1,0,1,0,0), c(0,0,0,1,1,0,0,1,0,1,0,0), c(0,0,0,1,1,0,0,1,0,1,0,0), c(0,0,0,1,1,1,1,1,0,1,0,0), c(0,0,0,1,1,0,0,1,1,1,1,1)), nrow = 12 , ncol = 6)
-mycolor <- colorRampPalette(c('green', 'darkgreen'))
-heatmap(test, col = mycolor(100))
+
+test <- matrix(c(c(0,0,0,1,1,0,0,1,0,1,0,0),
+                 c(0,0,0,0,0,1,0,0,1,1,1,0),
+                 c(0,0,0,0,0,0,0,1,0,1,0,0),
+                 c(0,0,0,1,0,0,0,0,1,0,0,1),
+                 c(0,0,1,1,0,1,0,1,1,1,0,0),
+                 c(0,0,1,0,0,1,0,0,1,1,0,0),
+                 c(1,1,1,1,1,0,1,1,0,1,0,1),
+                 c(0,0,0,0,0,0,1,1,0,0,0,0),
+                 c(0,0,0,0,0,0,1,1,0,0,0,0),
+                 c(0,0,0,0,1,0,0,1,0,1,0,1),
+                 c(0,0,0,0,0,0,0,0,0,0,0,0),
+                 c(0,0,1,0,0,0,0,1,0,1,0,1),
+                 c(0,0,1,1,0,1,0,1,1,1,0,0),
+                 c(0,0,1,1,0,0,0,1,1,0,0,0),
+                 c(0,0,0,1,0,0,1,1,1,1,0,0),
+                 c(0,0,1,1,0,0,0,1,0,1,0,0),
+                 c(1,0,0,0,1,0,1,1,0,0,0,0),
+                 c(1,1,1,1,0,1,1,0,0,1,0,0),
+                 c(0,0,0,0,0,0,0,1,0,0,0,0),
+                 c(1,1,1,0,0,0,1,1,1,1,1,1),
+                 c(1,0,0,0,1,1,0,0,1,1,1,1),
+                 c(0,1,1,0,1,1,0,1,1,1,0,0),
+                 c(0,0,1,1,1,1,0,1,0,0,0,0),
+                 c(0,0,0,1,0,1,0,1,1,1,0,1),
+                 c(0,0,0,0,0,0,0,0,0,1,0,0),
+                 c(0,0,0,0,1,1,1,0,0,1,0,0),
+                 c(0,0,1,0,0,0,0,1,1,1,0,0)),
+               nrow = 12, ncol = 27)
+
+colnames(test) <- c("MHP7448_RS00135 to MFC_RS03415", "MHP7448_RS00160 to MFC_RS02380", "MHP7448_RS00735 to MFC_RS01840", "MHP7448_RS00780 to MFC_RS01885", "MHP7448_RS00895 to MFC_RS02000", "MHP7448_RS00965 to MFC_RS02075", "MHP7448_RS01125 to MFC_RS02215", "MHP7448_RS01645 to MFC_RS00940",	"MHP7448_RS01695 to MFC_RS03005", "MHP7448_RS01760 to MFC_RS02360", "MHP7448_RS01830 to MFC_RS02980", "MHP7448_RS01965 to MFC_RS01350", "MHP7448_RS02430 to MFC_RS03175", "MHP7448_RS02535 to MFC_RS02885", "MHP7448_RS02710 to Without ortholog", "MHP7448_RS02825 to MFC_RS01090", "MHP7448_RS02840 to MFC_RS01070", "MHP7448_RS02910 to MFC_RS01005",	"MHP7448_RS03080 to MFC_RS01430", "MHP7448_RS03310 to MFC_RS01635", "MHP7448_RS03360 to MFC_RS01680", "MHP7448_RS03395 to MFC_RS02845", "MHP7448_RS03465 to MFC_RS02780", "MHP7448_RS03555 to MFC_RS00335", "MHP7448_RS03865 to Without ortholog", "MHP7448_RS03870 to Without ortholog", "MHP7448_RS04050 to MFC_RS03340")
+rownames(test) <- 
+
+mycolor <- colorRampPalette(c('white', 'green'))
+heatmap(test, col = mycolor(2))
 test
 
+t(heatmap)
 # # # # # OthersTry # # # # PresenceAbsencePlot
 
 # Link: https://www.youtube.com/watch?v=zsY-dFuIWYs
