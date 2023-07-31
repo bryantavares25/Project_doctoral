46762c0ed1eccb2a28c35af277ac0714e3ba2d64
Bryan Tavares <bryantavares25@gmail.com>
2023-07-31 10:23:04 -0300

    Update Promoter_sequence_LettterToCode.py

--- Proteases_sequence_retrieval/Promoter_sequence_LettterToCode.py
+++ Proteases_sequence_retrieval/Promoter_sequence_LettterToCode.py
@@ -2,3 +2,20 @@
 
 # Letter to Code
 
+dn = {'A':0, 'T':1, 'C':2, 'G':3}
+
+seqtest = 'GAAAAAAAAAAATCTCG'
+
+c_seqtest = []
+
+print(dn)
+
+for i in range(len(seqtest)):
+    print(seqtest[i])
+
+    convertido = seqtest[i]
+
+    c_seqtest.append(dn[convertido])
+
+print(c_seqtest)
+print('Finalizado')
