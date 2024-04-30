afb91965e447b32eb5e4b3e942ecbe0f4d5d338d
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-04-30 16:48:45 -0300

    Update Marcelo.py

--- Marcelo.py
+++ Marcelo.py
@@ -1,5 +1,7 @@
 # # #
 
+# # # # # L I B R A R I E S # # # # #
+
 # in terminal pip install libraries
 from Bio import SeqIO
 import csv
@@ -16,7 +18,7 @@ def tsv_read(archive):
     return data
 
 # Function definition
-def tsv_select(read, ref_list):
+def tsv_selection(read, ref_list):
     select = []
     test = []
     for line in read:
@@ -31,14 +33,25 @@ def tsv_write(data):
         escritor = csv.writer(arquivo_tsv, delimiter=',')  # Define o delimitador como tabulação
         escritor.writerows(dados)  # Escreve todos os dados
 
-# # #
+# # # # # E X E C U T I O N # # # # #
 
-file_initial = "/home/lgef/Documentos/banco.csv"
+# STEP - Select target IDs: tsv ids -> list
 
-select = []
+archive_initial = ""
+ids_selected = tsv_selection()
 
+# STEP - Read data: tsv data -> list
+
+archive_initial = "/home/lgef/Documentos/banco.csv"
+
+
+# STEP - Create tsv: tsv id + tsv data -> new archive 
 file_final = "/home/lgef/Documentos/novo.csv"
 
 dados = tsv_read(file_initial)
 
 tsv_write(dados)
+
+print("\n F I N A L I Z A D O \n")
+
+# # # # # # # # # # B. A. R. T. # # # # # # # # # #
