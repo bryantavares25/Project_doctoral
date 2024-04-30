fc1884cf8823afbede9576a32ac14328fd9297fa
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-04-30 17:00:13 -0300

    Update Marcelo.py

--- Marcelo.py
+++ Marcelo.py
@@ -2,7 +2,7 @@
 
 # # # # # L I B R A R I E S # # # # #
 
-# in terminal pip install libraries
+# In terminal pip install libraries
 from Bio import SeqIO
 import csv
 
@@ -28,6 +28,7 @@ def tsv_selection(read, ref_list):
     print(test)
     return select
 
+# Function definition
 def tsv_write(data):
     with open(file_final, 'w', newline='') as arquivo_tsv:
         escritor = csv.writer(arquivo_tsv, delimiter=',')  # Define o delimitador como tabulação
@@ -35,23 +36,24 @@ def tsv_write(data):
 
 # # # # # E X E C U T I O N # # # # #
 
-# STEP - Select target IDs: tsv ids -> list
+# STARTED
+print("\n S T A R T E D \n")
+
+# STEP 1 - Select target IDs: tsv ids -> list
 
 archive_initial = ""
-ids_selected = tsv_selection()
+ids_selected = tsv_read(archive_initial)
 
-# STEP - Read data: tsv data -> list
+# STEP 2 - Read data: tsv data -> list
 
 archive_initial = "/home/lgef/Documentos/banco.csv"
+data_selected = tsv_selection(archive_initial, ids_selected)
 
-
-# STEP - Create tsv: tsv id + tsv data -> new archive 
+# STEP 3 - Create tsv: tsv id + tsv data -> new archive 
 file_final = "/home/lgef/Documentos/novo.csv"
+tsv_write(data_selected)
 
-dados = tsv_read(file_initial)
-
-tsv_write(dados)
-
-print("\n F I N A L I Z A D O \n")
+# FINISHED
+print("\n F I N I S H E D \n")
 
 # # # # # # # # # # B. A. R. T. # # # # # # # # # #
