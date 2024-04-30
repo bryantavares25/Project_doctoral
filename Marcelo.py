2904a5c49a16ca9a0b76fa268cdef32221e93dc7
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-04-30 17:07:37 -0300

    Update Marcelo.py

--- Marcelo.py
+++ Marcelo.py
@@ -20,19 +20,27 @@ def tsv_read(archive):
 # Function definition
 def tsv_selection(read, ref_list):
     select = []
-    test = []
     for line in read:
-        if line[3] in ref_list:
+        if line[0] in ref_list:
             select.append(line[0])
-            test.append(line[3])
-    print(test)
+    print(select)
     return select
+
+    data = []
+    opened = open(archive, 'r', newline='', encoding='utf-8')
+    read_tsv = csv.reader(opened, delimiter='\t')
+    for line in read_tsv:
+        data.append(line)
+    return data
+
 
 # Function definition
 def tsv_write(data):
-    with open(file_final, 'w', newline='') as arquivo_tsv:
-        escritor = csv.writer(arquivo_tsv, delimiter=',')  # Define o delimitador como tabulação
-        escritor.writerows(dados)  # Escreve todos os dados
+    open
+
+    with open(archive_final, 'w', newline='') as archive_final:
+        escritor = csv.writer(archive_final, delimiter=',')  # Define o delimitador como tabulação
+        escritor.writerows(data)  # Escreve todos os dados
 
 # # # # # E X E C U T I O N # # # # #
 
@@ -41,16 +49,16 @@ print("\n S T A R T E D \n")
 
 # STEP 1 - Select target IDs: tsv ids -> list
 
-archive_initial = ""
+archive_initial = "/home/lgef/Documentos/Marcelo_tool/selecionar.csv"
 ids_selected = tsv_read(archive_initial)
 
 # STEP 2 - Read data: tsv data -> list
 
-archive_initial = "/home/lgef/Documentos/banco.csv"
+archive_initial = "/home/lgef/Documentos/Marcelo_tool/banco.csv"
 data_selected = tsv_selection(archive_initial, ids_selected)
 
 # STEP 3 - Create tsv: tsv id + tsv data -> new archive 
-file_final = "/home/lgef/Documentos/novo.csv"
+archive_final = "/home/lgef/Documentos/Marcelo_tool/novo.csv"
 tsv_write(data_selected)
 
 # FINISHED
