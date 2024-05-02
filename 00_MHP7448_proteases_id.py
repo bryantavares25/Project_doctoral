27ac0f882e8af4f6046f794e39c730ec141c4262
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-05-02 15:17:14 -0300

    Update

--- 00_MHP7448_proteases_id.py
+++ 00_MHP7448_proteases_id.py
@@ -15,9 +15,10 @@ def fasta_read(archive):
 # Function definition
 def fasta_create(features_list, file_output):
     file_output = open(file_output, "w")
-    for i in features_list:
-        #file_output.write(f"{i[1]}\n")
-        file_output.write(f">{i[0]}\n{i[1]}\n")
+    for i in features:
+       for l in prot_ids_mhp7448:
+            if l in i["des"]:
+                file_output.write(f">{i["id"]}\n{i["seq"]}\n")
     file_output.close()
 
 
@@ -30,15 +31,16 @@ print("\n S T A R T E D \n")
 # Data ids 
 prot_ids_mhp7448 = ["MHP7448_RS00135", "MHP7448_RS00160", "MHP7448_RS00735", "MHP7448_RS00780", "MHP7448_RS00895", "MHP7448_RS00965", "MHP7448_RS01125", "MHP7448_RS01645", "MHP7448_RS01695", "MHP7448_RS01760", "MHP7448_RS01830", "MHP7448_RS01965", "MHP7448_RS02430", "MHP7448_RS02535", "MHP7448_RS02710", "MHP7448_RS02825", "MHP7448_RS02840", "MHP7448_RS02910", "MHP7448_RS03080", "MHP7448_RS03310", "MHP7448_RS03360", "MHP7448_RS03395", "MHP7448_RS03465", "MHP7448_RS03555", "MHP7448_RS03865", "MHP7448_RS03870", "MHP7448_RS04050"]
 genome = "MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/cds_from_genomic.fna"
+save = "FASTA.fasta"
 
 features = fasta_read(genome)
 
-for i in features:
-    for l in prot_ids_mhp7448:
-        if l in i["des"]:
-            print(f"{i["id"]}")
-            print(f"{i["des"]}")
-            print(i["seq"])
+#for i in features:
+#    for l in prot_ids_mhp7448:
+#        if l in i["des"]:
+#            fasta_create(features, save)
+
+fasta_create(features, save)
 
 print("\n F I N I S H E D \n")
 
