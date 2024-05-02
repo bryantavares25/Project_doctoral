e0fe7a4d6ac571dd49ff503a516ef97df2acb823
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-05-02 15:28:02 -0300

    Update 00_MHP7448_proteases_id.py

--- 00_MHP7448_proteases_id.py
+++ 00_MHP7448_proteases_id.py
@@ -26,24 +26,25 @@ def fasta_create(features_list, file_output):
 
 print("\n S T A R T E D \n")
 
-# Read FASTA: genome to list
+# STEP - Read FASTA: genome to list
 
-# Data ids 
+# Input: Data ids 
 prot_ids_mhp7448 = ["MHP7448_RS00135", "MHP7448_RS00160", "MHP7448_RS00735", "MHP7448_RS00780", "MHP7448_RS00895", "MHP7448_RS00965", "MHP7448_RS01125", "MHP7448_RS01645", "MHP7448_RS01695", "MHP7448_RS01760", "MHP7448_RS01830", "MHP7448_RS01965", "MHP7448_RS02430", "MHP7448_RS02535", "MHP7448_RS02710", "MHP7448_RS02825", "MHP7448_RS02840", "MHP7448_RS02910", "MHP7448_RS03080", "MHP7448_RS03310", "MHP7448_RS03360", "MHP7448_RS03395", "MHP7448_RS03465", "MHP7448_RS03555", "MHP7448_RS03865", "MHP7448_RS03870", "MHP7448_RS04050"]
+# Input: Complete genome
 genome = "MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/cds_from_genomic.fna"
+# Output: archive fasta
 save = "FASTA.fasta"
 
 features = fasta_read(genome)
 
-#for i in features:
-#    for l in prot_ids_mhp7448:
-#        if l in i["des"]:
-#            fasta_create(features, save)
+# STEP - Create FASTA: list to fasta
 
 fasta_create(features, save)
 
 print("\n F I N I S H E D \n")
 
 # # # # # O B S
+
+# Check: OK
 
 # # # # # B. A. R. T. # # # # #
