94993f3812bb326117657c5cbd2d887ac652ceee
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-07-03 19:28:57 -0300

    Update

--- 00_DOC.py
+++ 00_DOC.py
@@ -2,14 +2,39 @@
 
 # LYBRARIES
 import csv
+# import pandas as pd
+from gtfparse import read_gtf
+# Substitua 'seu_arquivo.gtf' pelo caminho para o seu arquivo GTF
+df = read_gtf('/home/lgef/Documentos/GitHub/Project_doctoral/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/genomic.gff')
+# Exibir as primeiras linhas do dataframe
+l = []
+a = df[3]
+l.append(a)
+#l.append(df[3]['end'])
+#print(f"{df[3]['start']}")
+print(l)
+
 
 # FUNCTIONS
 
-def tsv_read(archive):    
+#def tsv_read(archive):
+    #data = []
+    #opened = open(archive, 'r', newline='', encoding='utf-8')
+    #read_tsv = csv.reader(opened, delimiter='\t')
+    #for line in read_tsv:
+    #    data.append(line)
+    #return data
+#def tsv_description_save(read_tsv_archive, i):
+    #description = []
+    #for line in read_tsv_archive:
+    #    if line[0] == i:
+    #        description.append(line[12])
+    #return description
 
 # EXECUTION
 
-
+#a = tsv_read('/home/lgef/Documentos/GitHub/Project_doctoral/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/genomic.gff')
+#print(a[9])
 
 # END
 
