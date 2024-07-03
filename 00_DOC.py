2ae396716ccdcab05d3afe038a36ea4347a3ac8a
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-07-03 20:55:28 -0300

    Update

--- 00_DOC.py
+++ 00_DOC.py
@@ -1,10 +1,14 @@
 # # # # # # # # # D O C # # # # # # # # #
 
+# RECOVERY BRUTE DATA TO CLEANED DATA
+# .gtf input to .tsv output
+# Recovery data: scaffold | start nucl | end nucl | strand | gene id code
+
 # # # LYBRARIES # # #
 import csv
 
 # # # FUNCTIONS # # #
-
+# Function definition
 def tsv_read(archive):
     data = []
     opened = open(archive, 'r', newline='', encoding='utf-8')
@@ -13,28 +17,26 @@ def tsv_read(archive):
         if line[2] == 'gene':
             data.append([line[0], line[3], line[4], line[6], line[8].split(';')[0]])
     return data
-def tsv_create(archive):
-
+# Function definition
+def tsv_create(data, archive):
+    write_tsv_archive = open(archive,"w")
+    for line in data:
+        for i in line:
+            write_tsv_archive.write(f"{i}\t")
+        write_tsv_archive.write(f"\n")
+    write_tsv_archive.close()
 
 # # # EXECUTION # # #
 
-file_input = "/home/lgef/Documentos/GitHub/Project_doctoral/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/genomic_cleaned.gtf"
+print(" - - - START - - -")
+# Input recipe
+file_input = '/home/lgef/Documentos/GitHub/Project_doctoral/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/genomic_cleaned.gtf'
 input = tsv_read(file_input)
-for i in input:
-    print(i)
-
+# Output creation
 file_output = "/home/lgef/Documentos/GitHub/Project_doctoral/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/genomic_cleaned.tsv"
-tsv_create(file_output)
+tsv_create(input, file_output)
+print(" - - - FINISHED - - - ")
 
 # END ------------------------------------
 
 # # # B A R T # # #
-
-# DRAF
-
-# import pandas as pd
-# from gtfparse import read_gtf
-# Substitua 'seu_arquivo.gtf' pelo caminho para o seu arquivo GTF
-# df = read_gtf('/home/lgef/Documentos/GitHub/Project_doctoral/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/genomic.gff')
-# Exibir as primeiras linhas do dataframe
-#print(f"{df[3]['start']}")
