5da7057d5009a2bc8c7555b8bd70a18bc728bf34
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-07-05 20:55:39 -0300

    Update 01_DOC_orthofinder.sh

--- 01_DOC_orthofinder.sh
+++ 01_DOC_orthofinder.sh
@@ -3,3 +3,16 @@
 genomes=(MHP_11 MHP_168)
 direc="/home/lgef/Documentos/GitHub/Project_doctoral/Genomes/Mesomycoplasma_hyopneumoniae"
 
+f="/home/lgef/Documentos/GitHub/Project_doctoral/Orthofinder_db"
+
+>WP_002557698.1 MULTISPECIES: serine protease [Mesomycoplasma]
+MKKFTKKYGFIGLFLALNLGILLANVALLTNNWIKLNPKVNLESLLKSIVEIKIQKNEEISFATGFVIDNKIITNKHILE
+NSDEIDIFYRLANEKDYKKTRIIKISQNYDILSLQLNTQVKNLEIEENFNYADEIFTIGNPHNLGLTISKGIISGTNKLA
+NQSFVRTSITIEPGNSGGPVLNTKNKVIGIMTFRLINEKPVQGISFFLPSSQIKEFLNSN
+
+conda activate fastqc
+# fastqc
+for i in "${seqid[@]}"; do fastqc ${direc}${i}/00_seq_ini/${i}_f* ${direc}${i}/00_seq_ini/${i}_r.* -o ${direc}${i}/01_seq_val/fastqc -t 15; done
+# multiqc
+for i in "${seqid[@]}"; do multiqc ${direc}${i}/01_seq_val/fastqc -o ${direc}${i}/01_seq_val; done
+conda deactivate
