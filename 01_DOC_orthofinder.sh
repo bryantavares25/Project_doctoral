47930640e789af5081b65ef7481a4acf3919e2d7
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-07-06 14:00:24 -0300

    Update 01_DOC_orthofinder.sh

--- 01_DOC_orthofinder.sh
+++ 01_DOC_orthofinder.sh
@@ -4,10 +4,10 @@ genomesmhp=(MHP_11 MHP_98 MHP_168 MHP_168L MHP_232 MHP_7422 MHP_7448 MHP_ES2 MHP
 genomesmfc=(MFC_ATCC27716 MFC_MF11 MFC_MF18 MFC_MF22 MFC_MF29 MFC_MF30 MFC_MF33 MFC_Ms42)
 
 direcgen="/home/lgef/Documentos/GitHub/Project_doctoral/"
-direcort=""
+direcort="dsada"
 
 #f="/home/lgef/Documentos/GitHub/Project_doctoral/Orthofinder_db"
-
+extension.extension.vim_cmd+cvim_cmd+c
 # Search MHP
 for genome in "${genomesmhp[@]}"; do cd cp ${direc}Genomes/Mesomycoplasma_hyopneumoniae/${genome}/ncbi_dataset/data/*/protein.faa ${direc}Orthofinder_db/${genome}_proteins_fasta; done
 # Search MFC
