db5d2b24bb9d68f0d8a5d3457a52eb42dc21a100
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-07-09 21:05:36 -0300

    Update 02_DOC_Orthorecovery.py

--- 02_DOC_Orthorecovery.py
+++ 02_DOC_Orthorecovery.py
@@ -5,22 +5,29 @@ from Bio import SeqIO
 
 # # # FUNCTIONS # # #
 
+# Function definition - READ FASTA
+def fasta_read(data, file_fasta):
+    features = []
+    for feature in SeqIO.parse(file_fasta, "fasta"):
+        if feature.id in data:
+            print(feature.id)
+            features.append({"id" : feature.id, "des" : feature.description, "seq" : feature.seq})
+    return features
+
 # Function definition
 def extract_region(fasta_file, start, end):
     sequences = []
     for record in SeqIO.parse(fasta_file, "fasta"):
-        sub_sequence = record.seq[start-1:end]  # Ajuste para a contagem baseada em 0
+        sub_sequence = record.seq[start-1:end]
         sequences.append((record.id, sub_sequence))
     return sequences
 
 # # # EXECUTION # # #
 
-# Parâmetros
-fasta_file = "home/lgef/Documentos/GitHub/Project_doctoral/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/GCF_000008225.1_ASM822v1_genomic.fna"   # Arquivo FASTA de entrada
+fasta_file = "home/lgef/Documentos/GitHub/Project_doctoral/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/GCF_000008225.1_ASM822v1_genomic.fasta"   # Arquivo FASTA de entrada
 start = 100 # Posição inicial (inclusive)
 end = 150 # Posição final (inclusive)
 
-# Extrair a região específica
 extracted_sequences = extract_region(fasta_file, start, end)
 
 # Imprimir as sequências extraídas
