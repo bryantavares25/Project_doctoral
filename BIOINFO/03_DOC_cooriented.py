0f95c50b20aa1cb4ebfcfda4b6e04576c9b20dc5
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-08-28 21:31:58 -0300

    Update

--- BIOINFO/03_DOC_cooriented.py
+++ BIOINFO/03_DOC_cooriented.py
@@ -5,29 +5,45 @@
 
 import pandas as pd
 
-file = 'BIOINFO/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/genomic_gff_cleaned.tsv'
-df = pd.read_csv(file, sep='\t', header=None, names=['seqname', 'start', 'end', 'strand', 'ID', 'Parent', 'Teste'])
+def tsv_create(data, archive):
+    write_tsv_archive = open(archive,"w")
+    for line in data:
+        for i in line:
+            write_tsv_archive.write(f"{i}\t")
+        write_tsv_archive.write(f"\n")
+    write_tsv_archive.close()
+
+file = 'BIOINFO/MHP_7448_dataset/ncbi_dataset/data/GCF_000008225.1/genomic_cleaned_final.csv'
+df = pd.read_csv(file, sep='\t', header=None, names=['seqname', 'start', 'end', 'strand', 'gene', 'ID'])
 
 co_oriented_groups = []
-current_group = [df.iloc[0]['Parent']]
+current_group = [df.iloc[0]['ID']]
 current_strand = df.iloc[0]['strand']
 
 for i in range(1, len(df)):
-    if df.iloc[i]['strand'] == current_strand:
-        current_group.append(df.iloc[i]['Parent'])
-        print(current_group)
+    if df.iloc[i]['strand'] == current_strand: #and df.iloc[i]['start'] >= df.iloc[i-1]['end']:
+        current_group.append(df.iloc[i]['ID'])
+
     else:
         if len(current_group) >= 1:
             co_oriented_groups.append(current_group)
-        current_group = [df.iloc[i]['Parent']]
+        current_group = [df.iloc[i]['ID']]
         current_strand = df.iloc[i]['strand']
+
 
 if len(current_group) >= 1:
     co_oriented_groups.append(current_group)
 
-a = 0
-for group in co_oriented_groups:
-    a = a+1
-    print(f"Grupo de genes co-orientados: {', '.join(group)}")
-print(a)
+
+# Exibe os grupos de genes co-orientados
+a = []
+prot_ids_mhp7448 = ['MHP7448_RS00135', 'MHP7448_RS00160', 'MHP7448_RS00735', 'MHP7448_RS00780', 'MHP7448_RS00895', 'MHP7448_RS00965', 'MHP7448_RS01125', 'MHP7448_RS01645', 'MHP7448_RS01695', 'MHP7448_RS01760', 'MHP7448_RS01830', 'MHP7448_RS01965', 'MHP7448_RS02430', 'MHP7448_RS02535', 'MHP7448_RS02710', 'MHP7448_RS02825', 'MHP7448_RS02840', 'MHP7448_RS02910', 'MHP7448_RS03080', 'MHP7448_RS03310', 'MHP7448_RS03360', 'MHP7448_RS03395', 'MHP7448_RS03465', 'MHP7448_RS03555', 'MHP7448_RS03865', 'MHP7448_RS03870', 'MHP7448_RS04050']
+for i in prot_ids_mhp7448:
+    for group in co_oriented_groups:
+        if i in group:
+            print(group)
+            a.append([i, group])
+
+archive = '/home/lgef/Documentos/GitHub/Project_doctoral/BIOINFO/03_outputclusters.tsv'
+tsv_create(a, archive)
 
