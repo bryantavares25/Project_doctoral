1e9c89923c8711ff5d57c8764a2d49885fe32133
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-11-25 17:38:30 -0300

    Update 00_organization.bash

--- IMPLEMENTAÇÃO/Code/00_organization.bash
+++ IMPLEMENTAÇÃO/Code/00_organization.bash
@@ -10,6 +10,9 @@ ids=()
 direc=/home/lgef/Documents/GitHub/Project_doctoral/IMPLEMENTAÇÃO/
 # /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Code/00_organization.bash
 
-for i in "${seqid[@]}"; do mkdir -p {$direc}{$ids}; mv {$direc}Genomes/M_hyopneumoniae/MHP_ncbi_dataset/ncbi_dataset/data/GCA_000008205.1; done
+for i in "${seqid[@]}"; do mkdir -p {$direc}{$ids}; done
+
+
+for i in ; do mv {$direc}Genomes/M_hyopneumoniae/MHP_ncbi_dataset/ncbi_dataset/data/**GCA_000008205.1**; done
 
 
