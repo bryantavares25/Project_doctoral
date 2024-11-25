911c6b6b82f40e23b60d7ab5aace2f9af7c7aa2a
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-11-25 17:25:38 -0300

    Update 00_organization.bash

--- IMPLEMENTAÇÃO/Code/00_organization.bash
+++ IMPLEMENTAÇÃO/Code/00_organization.bash
@@ -5,3 +5,9 @@
 # Create folder by species
 
 awk 'NR > 1 && !seen[$5]++ {print $5}' IMPLEMENTAÇÃO/Genomes/mfc_table.tsv > IMPLEMENTAÇÃO/Genomes/mfc_list.txt
+
+ids=()
+direc=/home/lgef/Documents/Projects
+# /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Code/00_organization.bash
+
+for i in "${seqid[@]}"; do ; done
