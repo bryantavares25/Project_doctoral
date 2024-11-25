63fb77d8dcec92f987a669465781b588ffd0c0ad
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-11-25 20:49:19 -0300

    Update

--- IMPLEMENTAÇÃO/Code/00_organization.bash
+++ IMPLEMENTAÇÃO/Code/00_organization.bash
@@ -7,12 +7,15 @@
 awk -F'\t' 'NR > 1 && !seen[$5]++ {print $5}' IMPLEMENTAÇÃO/Genomes/mhp_table.tsv > IMPLEMENTAÇÃO/Genomes/mhp_list.txt
 awk -F'\t' 'NR > 1 && !seen[$5]++ {print $5}' IMPLEMENTAÇÃO/Genomes/mfc_table.tsv > IMPLEMENTAÇÃO/Genomes/mfc_list.txt
 
-awk -F'\t' 'NR > 1 && !seen[$1] {print $1}' IMPLEMENTAÇÃO/Genomes/mhp_table.tsv > IMPLEMENTAÇÃO/Genomes/mhp_ids.txt
-awk -F'\t' 'NR > 1 && !seen[$1]++ {print $1}' IMPLEMENTAÇÃO/Genomes/mfc_table.tsv > IMPLEMENTAÇÃO/Genomes/mfc_ids.txt
+#awk -F'\t' 'NR > 1 && !seen[$1] {print $1}' IMPLEMENTAÇÃO/Genomes/mhp_table.tsv > IMPLEMENTAÇÃO/Genomes/mhp_ids.txt
+#awk -F'\t' 'NR > 1 && !seen[$1]++ {print $1}' IMPLEMENTAÇÃO/Genomes/mfc_table.tsv > IMPLEMENTAÇÃO/Genomes/mfc_ids.txt
 
 ids=()
 direc=/home/lgef/Documents/GitHub/Project_doctoral/IMPLEMENTAÇÃO/
 # /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Code/00_organization.bash
+
+awk '{ printf "%s ", $0 } END { print "" }' IMPLEMENTAÇÃO/Genomes/mhp_list.txt > IMPLEMENTAÇÃO/Genomes/mhp_ids.txt
+awk '{ printf "%s ", $0 } END { print "" }' IMPLEMENTAÇÃO/Genomes/mfc_list.txt > IMPLEMENTAÇÃO/Genomes/mfc_ids.txt
 
 for i in "${seqid[@]}"; do mkdir -p {$direc}{$ids}; done
 
