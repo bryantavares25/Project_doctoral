404db236187cec555bf0029347c83423b466608e
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-11-25 22:40:15 -0300

    Update

--- BIOINFO_TEST/Proteases_sequence_retrieval/README.md
+++
@@ -1,8 +0,0 @@
-# Sequence_retrieval
- 
-Pipeline: 
-
-Input 1: genome sequence in .fasta
-Input 2: start and end position .txt or .csv
-
-Output 1: interval sequence in .fasta
--- IMPLEMENTAÇÃO/Code/00_organization.bash
+++ IMPLEMENTAÇÃO/Code/00_organization.bash
@@ -2,27 +2,47 @@
 
 # Organization
 
-# Create folder by species
+# Folders
+pcH=/home/bryan/
+pcL=/home/lgef/
+direc_mhp=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Genomes/M_hyopneumoniae/
+direc_mfc=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Genomes/M_flocculare/
+mhp_table="IMPLEMENTAÇÃO/Genomes/mhp_table.tsv"
+mhp_list="IMPLEMENTAÇÃO/Genomes/mhp_list.txt"
+mhp_temp="IMPLEMENTAÇÃO/Genomes/mhp_result.txt"
+mfc_table="IMPLEMENTAÇÃO/Genomes/mfc_table.tsv"
+mfc_list="IMPLEMENTAÇÃO/Genomes/mfc_list.txt"
+mfc_temp="IMPLEMENTAÇÃO/Genomes/mfc_result.txt"
+
 # In collumn
-awk -F'\t' 'NR > 1 && !seen[$5]++ {print $5}' IMPLEMENTAÇÃO/Genomes/mhp_table.tsv > IMPLEMENTAÇÃO/Genomes/mhp_list.txt
-awk -F'\t' 'NR > 1 && !seen[$5]++ {print $5}' IMPLEMENTAÇÃO/Genomes/mfc_table.tsv > IMPLEMENTAÇÃO/Genomes/mfc_list.txt
+awk -F'\t' 'NR > 1 && !seen[$5]++ {print $5}' "$mhp_table" > "$mhp_list"
+awk -F'\t' 'NR > 1 && !seen[$5]++ {print $5}' "$mfc_table" > "$mfc_list"
 
-direc_mhp=/home/lgef/Documents/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Genomes/M_hyopneumoniae
-direc_mfc=/home/lgef/Documents/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Genomes/M_flocculare
-
-while IFS= read -r line; do a=$line; mkdir -p {$direc}{$a}; done < "IMPLEMENTAÇÃO/Genomes/mhp_list.txt"
-while IFS= read -r line; do a=$line; mkdir -p {$direc}{$a}; done < "IMPLEMENTAÇÃO/Genomes/mfc_list.txt"
-
-# # # # # # #
+# Create folder by species
+while IFS= read -r line; do a=$line; mkdir -p ${direc_mhp}${a}/Use; done < "$mhp_list"
+while IFS= read -r line; do a=$line; mkdir -p ${direc_mfc}${a}/Use; done < "$mfc_list"
 
 # Move files
-while IFS= read -r line; do a=$line; /home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Genomes/mhp_list.txt; done < "IMPLEMENTAÇÃO/Genomes/mhp_list.txt"
+while IFS= read -r line; do
+    a=$line
+    awk -v id="$a" '$5 == id { print $1}' "$input_file" > "$output_file"
+    while IFS= read -r line; do
+        echo $line
+        b=$line
+        mv ${i}MHP_ncbi_dataset/ncbi_dataset/data/${line} ${direc_mhp}${a}
+    done < "$output_file"
+done < "$mhp_list"
+
+while IFS= read -r line; do
+    a=$line
+    awk -v id="$a" '$5 == id { print $1}' "$input_file" > "$output_file"
+    while IFS= read -r line; do
+        echo $line
+        b=$line
+        mv ${direc_mfc}MFC_ncbi_dataset/ncbi_dataset/data/${line} ${direc_mfc}${a}
+    done < "$output_file"
+done < "$mfc_list"
 
 # /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Code/00_organization.bash
-for i in "${seqid[@]}"; do ; done
-for i in ; do mv {$direc}Genomes/M_hyopneumoniae/MHP_ncbi_dataset/ncbi_dataset/data/**GCA_000008205.1**; done
-
-input_file="/home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Genomes/mhp_table.tsv"
-id="7448"
-
-awk -v id="$id" '$1 ~ id { print }' "$input_file"
+#for i in "${seqid[@]}"; do ; done
+#for i in ; do mv {$direc}Genomes/M_hyopneumoniae/MHP_ncbi_dataset/ncbi_dataset/data/**GCA_000008205.1**; done
