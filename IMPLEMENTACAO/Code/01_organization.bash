9d39e3c39d46f262a286ccdd47bed08331de143d
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-11-26 20:06:37 -0300

    Update 01_organization.bash

--- IMPLEMENTACAO/Code/01_organization.bash
+++ IMPLEMENTACAO/Code/01_organization.bash
@@ -14,19 +14,30 @@ mfc_table="IMPLEMENTACAO/Genomes/mfc_table.tsv" # Curadoria manual
 mfc_list="IMPLEMENTACAO/Genomes/mfc_list.txt"
 mfc_temp="IMPLEMENTACAO/Genomes/mfc_result.txt"
 
-if exist GCF** cp GCF; elif exist GCA** cp GCA
-
 mhp=/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/MHP_ncbi_dataset/ncbi_dataset/data/strains/
+for file in "${mhp}*/"; do
+    echo $file
+    if ls ${file}GCF* 1> /dev/null 2>&1; then
+        echo $file
+        #cp -r ${file}GCF* ${file}Use/
+    elif ls ${file}GCA* 1> /dev/null 2>&1; then
+        echo "NO"
+        #cp -r ${file}GCA* ${file}Use/
+    else
+        echo "Nenhum arquivo GCF ou GCA encontrado."
+    fi
+    echo "\n"
+done
+
 mfc=/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/MFC_ncbi_dataset/ncbi_dataset/data/strains/
-
-for subdir in "${mfc[@]}"; do echo $subdir; done
-
-for file in $mfc"${seqid[@]}"
-
-if ls ${mfc}ATCC27716/GCF* 1> /dev/null 2>&1; then
-    cp -r ${mfc}ATCC27716/GCF* ${mfc}ATCC27716/Use/
-elif ls ${mfc}ATCC27716/GCA* 1> /dev/null 2>&1; then
-    cp -r ${mfc}ATCC27716/GCA* ${mfc}ATCC27716/Use/
-else
-    echo "Nenhum arquivo GCF ou GCA encontrado."
-fi
+for file in "${mfc[@]}*/"; do
+    if ls ${file}ATCC27716/GCF* 1> /dev/null 2>&1; then
+        echo "OK"
+        #cp -r ${file}ATCC27716/GCF* ${file}ATCC27716/Use/
+    elif ls ${file}ATCC27716/GCA* 1> /dev/null 2>&1; then
+        echo "NO"
+        #cp -r ${file}ATCC27716/GCA* ${file}ATCC27716/Use/
+    else
+        echo "Nenhum arquivo GCF ou GCA encontrado."
+    fi
+done
