7b505539b07d2ed5bc9e7ace6e7a61526d019be1
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-11-25 21:31:34 -0300

    Update

--- IMPLEMENTAÇÃO/Code/00_organization.bash
+++ IMPLEMENTAÇÃO/Code/00_organization.bash
@@ -16,8 +16,13 @@ while IFS= read -r line; do a=$line; mkdir -p {$direc}{$a}; done < "IMPLEMENTAÇ
 # # # # # # #
 
 # Move files
+while IFS= read -r line; do a=$line; /home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Genomes/mhp_list.txt; done < "IMPLEMENTAÇÃO/Genomes/mhp_list.txt"
 
 # /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Code/00_organization.bash
-for i in "${seqid[@]}"; do mkdir -p {$direc}{$ids}; done
+for i in "${seqid[@]}"; do ; done
 for i in ; do mv {$direc}Genomes/M_hyopneumoniae/MHP_ncbi_dataset/ncbi_dataset/data/**GCA_000008205.1**; done
 
+input_file="/home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTAÇÃO/Genomes/mhp_table.tsv"
+id="7448"
+
+awk -v id="$id" '$1 ~ id { print }' "$input_file"
