5d9ebdd0c7d108bd0a861a4c5c5ddf20245a324f
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-11-26 21:08:46 -0300

    Create 02_organization.bash

---
+++ IMPLEMENTACAO/Code/02_organization.bash
@@ -0,0 +1,18 @@
+# # # # #
+
+# Organization USE
+
+# Folders
+pcH=/home/bryan/
+pcL=/home/lgef/
+direc_mhp=${pcL}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/MHP_ncbi_dataset/ncbi_dataset/data/
+direc_mfc=${pcL}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/MFC_ncbi_dataset/ncbi_dataset/data/
+mhp_table="IMPLEMENTACAO/Genomes/mhp_table.tsv" # Curadoria manual
+mhp_list="IMPLEMENTACAO/Genomes/mhp_list.txt"
+mhp_temp="IMPLEMENTACAO/Genomes/mhp_result.txt"
+mfc_table="IMPLEMENTACAO/Genomes/mfc_table.tsv" # Curadoria manual
+mfc_list="IMPLEMENTACAO/Genomes/mfc_list.txt"
+mfc_temp="IMPLEMENTACAO/Genomes/mfc_result.txt"
+
+#
+
