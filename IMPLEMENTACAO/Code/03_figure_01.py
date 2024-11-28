032ceec538cb406f6447cf15a5d18341f7b6e102
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-11-28 20:03:44 -0300

    Update 03_figure_01.py

--- IMPLEMENTACAO/Code/03_figure_01.py
+++ IMPLEMENTACAO/Code/03_figure_01.py
@@ -5,5 +5,25 @@
 '''Figura 1 - Comparar propriedades genômicos para avaliar os genomas completos e os genomas completados.
 Propriedades:
     - Conteúdo GC 
-    -
+    - Completude Busco Result
+    - Cromossomos
+    - Tamanho
+    - Quantidade de Ns
 '''
+
+import matplotlib.pyplot as plt
+
+# Dados para o gráfico
+categorias = ['A', 'B', 'C', 'D']
+valores = [10, 20, 15, 25]
+
+# Criando o gráfico de barras
+plt.bar(categorias, valores, color='blue')
+
+# Personalização do gráfico
+plt.title('Gráfico de Barras com Matplotlib')
+plt.xlabel('Categorias')
+plt.ylabel('Valores')
+
+# Exibindo o gráfico
+plt.show()
