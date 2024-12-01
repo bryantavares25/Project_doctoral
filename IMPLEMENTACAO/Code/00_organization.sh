e61a81ffc523556408e89cfa5197cda27b502c7a
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-12-01 16:48:15 -0300

    Update 00_organization.sh

--- IMPLEMENTACAO/Code/00_organization.sh
+++ IMPLEMENTACAO/Code/00_organization.sh
@@ -8,8 +8,8 @@
 # Folders
 pcH=/home/bryan/
 pcL=/home/lgef/
-direc_mhp=${pcL}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/MHP_ncbi_dataset/ncbi_dataset/data/
-direc_mfc=${pcL}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/MFC_ncbi_dataset/ncbi_dataset/data/
+direc_mhp=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/
+direc_mfc=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/
 mhp_table="IMPLEMENTACAO/Genomes/mhp_table.tsv" # Curadoria manual
 mhp_list="IMPLEMENTACAO/Genomes/mhp_list.txt"
 mhp_temp="IMPLEMENTACAO/Genomes/mhp_result.txt"
