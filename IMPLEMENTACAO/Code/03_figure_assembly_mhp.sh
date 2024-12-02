209de45482ae94c04fd45c7cb9015e1642502ad0
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-12-02 13:59:55 -0300

    Update 03_figure_assembly_mhp.sh

--- IMPLEMENTACAO/Code/03_figure_assembly_mhp.sh
+++ IMPLEMENTACAO/Code/03_figure_assembly_mhp.sh
@@ -7,16 +7,17 @@ conda init bash
 # Folders
 pcH=/home/bryan/
 pcL=/home/lgef/
-direc_mhp=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/
-direc_mfc=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/
-mhp_strains=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/strains/
-mfc_strains=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/strains/
-mhp_table=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mhp_table.tsv # Curadoria manual
-mhp_list=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mhp_list.txt
-mhp_temp=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mhp_result.txt
-mfc_table=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_table.tsv # Curadoria manual
-mfc_list=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_list.txt
-mfc_temp=${pcH}Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_result.txt
+pcR=/home/regenera/
+direc_mhp=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/
+direc_mfc=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/
+mhp_strains=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/strains/
+mfc_strains=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/strains/
+mhp_table=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mhp_table.tsv # Curadoria manual
+mhp_list=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mhp_list.txt
+mhp_temp=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mhp_result.txt
+mfc_table=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_table.tsv # Curadoria manual
+mfc_list=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_list.txt
+mfc_temp=${pcR}Documents/BART_gitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_result.txt
 
 # # # DATA COLLECT # # #
 
