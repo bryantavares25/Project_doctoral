26b96a3e08541408e5cb3d4a9acc66fd4d8392b3
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-12-09 09:12:03 -0300

    Organization

--- IMPLEMENTACAO/Code/03_figure_01A_mfc.py
+++ IMPLEMENTACAO/Code/03_figure_01A_mfc.py
@@ -1,4 +1,4 @@
-# # # F I G U R E S # # #
+'''# # # F I G U R E S # # #
 
 #import pylustrator
 import matplotlib.pyplot as plt
@@ -8,6 +8,8 @@ import csv
 
 # FIGURA 1
 
+'''
+
 '''Figura 1 - Comparar propriedades genômicos para avaliar os genomas completos e os genomas completados.
 Propriedades:
     - Conteúdo GC 
@@ -16,7 +18,7 @@ Propriedades:
     - Tamanho
     - Quantidade de Ns
 '''
-
+'''
 def tsv_read(archive):
     data = []
     opened = open(archive, 'r', newline='', encoding='utf-8')
@@ -27,7 +29,7 @@ def tsv_read(archive):
 
 
 # FIGURE 1 A
-data=tsv_read("/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/Stats_DtC/scaffolds.tsv")
+data=tsv_read("/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/stats_DtC/scaffolds.tsv")
 category=data[0]
 print(category)
 values1=list(map(int, data[1]))  # Converte para int
@@ -50,11 +52,64 @@ ax.set_xticklabels(category, rotation=45, ha='right')
 ax.tick_params(axis='x', labelsize=10)
 ax.legend()
 # Exibição
+
+y = np.arange(len(category))  # Posições no eixo X
+largura = 0.35  # Largura das barras
+fig, ay = plt.subplots() # Criação do gráfico
+barras1 = ay.bar(y - largura/2, values1, largura, label='Draft', color='skyblue')
+barras2 = ay.bar(y + largura/2, values2, largura, label='Whole', color='salmon')
+# Personalização do gráfico
+ay.set_xlabel('M. hyopneumoniae strains')
+ay.set_ylabel('Scaffolds')
+ay.set_title('Gráfico de Barras Lado a Lado')
+ay.set_xticks(y)
+ay.set_xticklabels(category, rotation=45, ha='right')
+ay.tick_params(axis='x', labelsize=10)
+ay.legend()
+
+plt.show() #plt.tight_layout()
+'''
+
+import pylustrator
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Ativar o pylustrator
+pylustrator.start()
+
+# Dados de exemplo
+x = np.linspace(0, 10, 100)
+y1 = np.sin(x)
+y2 = np.cos(x)
+
+# Criar uma figura com subplots
+fig, axs = plt.subplots(2, 2, figsize=(10, 8))
+
+# Primeiro gráfico
+axs[0, 0].plot(x, y1, label="Seno")
+axs[0, 0].set_title("Gráfico 1")
+axs[0, 0].legend()
+
+# Segundo gráfico
+axs[0, 1].plot(x, y2, label="Cosseno", color="red")
+axs[0, 1].set_title("Gráfico 2")
+axs[0, 1].legend()
+
+# Terceiro gráfico
+axs[1, 0].plot(x, y1 * y2, label="Seno * Cosseno", color="green")
+axs[1, 0].set_title("Gráfico 3")
+axs[1, 0].legend()
+
+# Quarto gráfico
+axs[1, 1].plot(x, y1 - y2, label="Seno - Cosseno", color="purple")
+axs[1, 1].set_title("Gráfico 4")
+axs[1, 1].legend()
+
+# Ajustar espaçamento entre subplots
 plt.tight_layout()
-#% start: automatic generated code from pylustrator
-plt.figure(1).ax_dict = {ax.get_label(): ax for ax in plt.figure(1).axes}
-import matplotlib as mpl
-getattr(plt.figure(1), '_pylustrator_init', lambda: ...)()
-#% end: automatic generated code from pylustrator
+
+# Quando o script é executado, a janela do pylustrator é aberta.
 plt.show()
 
+# Salvar a configuração final interativa:
+# pylustrator.export("output_figure.png")  # Salva como PNG
---
+++ IMPLEMENTACAO/Code/03_figure_01_all.py
@@ -0,0 +1,13 @@
+# # # # # M A N U S C R I P T - F I G U R E S # # # # #
+
+# # # F I G U R E - 01 # # #
+
+# LIBRARIES
+
+#import pylustrator
+import matplotlib.pyplot as plt
+import numpy as np
+import csv
+#pylustrator.start()
+
+# 
