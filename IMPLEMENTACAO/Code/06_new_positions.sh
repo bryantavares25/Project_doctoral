435e11a3e95732c2d853ae4b31038ca50870f6a0
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-12-11 11:54:25 -0300

    Update

--- IMPLEMENTACAO/Code/06_new_positions.sh
+++ IMPLEMENTACAO/Code/06_new_positions.sh
@@ -2,5 +2,21 @@
 
 conda init bash
 
-# # # #
+# START > > > ZERO
 
+# ARCHIVE
+dir=/home/lgef
+#dir=/home/bryan
+
+# INPUT FILE
+input_gff=
+
+# EXECUTION
+
+# Recovery from .gff: seqregion | ID | gene_biotype | strand | start | stop
+
+# Create the .txt: seqregion | start | stop
+
+# Recovery from .fna with base from .txt and ID: >ID+fasta sequence
+
+# END
