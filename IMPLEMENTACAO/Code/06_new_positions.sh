cd6edbe041f26ce59080ae55686588e23066e881
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2024-12-16 09:49:38 -0300

    Update

--- IMPLEMENTACAO/Code/06_new_positions.sh
+++ IMPLEMENTACAO/Code/06_new_positions.sh
@@ -50,7 +50,7 @@ awk '/^>/ {if (seq) {print seq > output_file}; seq=""; output_file=substr($0,2)"
 sequence=$(awk '!/^>/' "$input_file")
 echo "${sequence[@]}" # Exibir a sequência
 for i in $sequence; do
-    seqkit locate -i -p "$i" /home/lgef/Documentos/GitHub/Project_doctoral/BIOINFO_TEST/11/ALL_GCF_002193015.fna
+    seqkit locate -i -p "$i" /home/lgef/Documentos/GitHub/Project_doctoral/BIOINFO_TEST/11/ALL_GCF_002193015.fna >> /home/lgef/Documentos/GitHub/Project_doctoral/BIOINFO_TEST/11/ALL_FASTA.fasta
 done
 #### OUTPUT : FASTA SEQ
 
