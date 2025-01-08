441a99eabbe5c4869c0c1375ab1c6da4232ab731
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-01-08 10:58:11 -0300

    Update

--- IMPLEMENTACAO/Code/07_new_positions_mhp.sh
+++ IMPLEMENTACAO/Code/07_new_positions_mhp.sh
@@ -19,20 +19,6 @@ mhp_genome_new=$direc_mhp/mult_align/seqs_to_align/$file.fasta
 mhp_genes_location=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/$file/genes_location.tsv
 mhp_genes_location_clean=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/$file/genes_location_clean.tsv
 
-
-teste=/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/11/gff_data.tsv
-testes=/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/11/gff_data_to_map.tsv
-testes_juntos=/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/11/gff_juntos.tsv
-# limpar ALL_FASTA.fasta # CURADORIA MANUAL # a ordem seguida é do arquivo .gff (podemos melhorar a confiabilidade)
-
-genes_location_clean.fasta=$dir
-ALL_FASTA_CLEANED_AWK.tsv=
-ALL_FASTA_CLEANED_AWK.tsv=
-teste_03.tsv=
-arquivo_junto.tsv=
-genomic.gff=
-genomic_novo.gff=
-
 for file in $(cat $mhp_list); do
     # Folders
     mhp_gff_data=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/$file/gff_data.tsv
@@ -40,7 +26,8 @@ for file in $(cat $mhp_list); do
     mhp_genes_location_clean=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/$file/genes_location_clean.tsv
     mhp_genes_location_clean_strand=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/$file/genes_location_clean_strand.tsv
     mhp_genes_location_strand_complete=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/$file/genes_strand_complete.tsv
-    $mhp_genes_location_strand_final=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/$file/genes_strand_final.tsv
+    mhp_genes_location_strand_final=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/$file/genes_strand_final.tsv
+    mhp_genes_map=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/$file/genes_map.tsv
 
     # Criar arquivos para estabelecer a strand correta
     awk 'BEGIN {OFS="\t"} {print $1, $2, $3, $4}' $mhp_gff_data > $mhp_gff_strand
@@ -58,27 +45,25 @@ for file in $(cat $mhp_list); do
         }
     }' $mhp_genes_location_strand_complete > $mhp_genes_location_strand_final
 
+    # Criando mapa
+    awk 'BEGIN {OFS="\t"} {print $1, $2, $3, $4, $5, $9, $6, $7, $8' $mhp_genes_location_strand_final > $mhp_genes_map
 
-
-# Construir MAPA # mapa.tsv > > > NZ_MWWN01000001.1 1 1624 NZ_MWWN01000002.1 2 1625
-paste ALL_FASTA_CLEANED_AWK.tsv teste_03.tsv > arquivo_junto.tsv
-
-# Substituição da localização
-awk 'BEGIN { OFS="\t" }
-    NR==FNR {
-        map[$1 FS $2 FS $3] = $4 FS $5 FS $6
-        next
-    }
-    {
-        key = $1 FS $4 FS $5
-        if (key in map) {
-            split(map[key], new_values, FS)
-            $1 = new_values[1]
-            $4 = new_values[2]
-            $5 = new_values[3]
+    # Substituição da localização
+    awk 'BEGIN { OFS="\t" }
+        NR==FNR {
+            map[$1 FS $2 FS $3] = $4 FS $5 FS $6
+            next
         }
-        print
-    }' mapa.tsv genomic.gff > genomic_novo.gff
+        {
+            key = $1 FS $4 FS $5
+            if (key in map) {
+                split(map[key], new_values, FS)
+                $1 = new_values[1]
+                $4 = new_values[2]
+                $5 = new_values[3]
+            }
+            print
+        }' mapa.tsv genomic.gff > genomic_novo.gff
 
 # END > > > 
 # GENOMES TO GENE_CLUSTERS
