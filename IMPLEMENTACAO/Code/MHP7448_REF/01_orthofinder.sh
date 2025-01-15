8455e3fc3109eaf1148e67d79c58528090536ca4
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-01-15 11:36:32 -0300

    Update

--- IMPLEMENTACAO/Code/MHP7448_REF/01_orthofinder.sh
+++ IMPLEMENTACAO/Code/MHP7448_REF/01_orthofinder.sh
@@ -5,27 +5,29 @@ dir=/home/lgef
 
 #/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/Orthofinder
 input_folder=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/input_proteins/
+output_folder=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/output_proteins
+
 
 direc_mfc=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare
 mfc_list=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_list.txt
 for file in $(cat $mfc_list); do
     cd ${direc_mfc}/strains/${file}/Use/G*.1 # OK
-    cp protein.faa ${file}_proteins_mfc.fasta
+    cp protein.faa MFC_${file}_proteins.fasta
     #rm ${file}_proteins.fasta
     cd
-    mv ${direc_mfc}/strains/${file}/Use/G*.1/${file}_proteins_mfc.fasta $input_folder
+    mv ${direc_mfc}/strains/${file}/Use/G*.1/MFC_${file}_proteins.fasta $input_folder
 done
 
 direc_mhp=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae
 mhp_list=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mhp_list.txt
 for file in $(cat $mhp_list); do
     cd ${direc_mhp}/strains/${file}/Use/G*.1 # OK
-    cp protein.faa ${file}_proteins_mhp.fasta
+    cp protein.faa MHP_${file}_proteins.fasta
     #rm ${file}_proteins.fasta
     cd
-    mv ${direc_mhp}/strains/${file}/Use/G*.1/${file}_proteins_mhp.fasta $input_folder
+    mv ${direc_mhp}/strains/${file}/Use/G*.1/MHP_${file}_proteins.fasta $input_folder
 done
 
 # # # END > > > RUN ORTHOFINDER
 
-#
+# orthofinder -f $input_folder -o $output_folder
