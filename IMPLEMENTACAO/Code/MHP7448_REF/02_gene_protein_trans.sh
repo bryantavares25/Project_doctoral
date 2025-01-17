45af9779b10c49576ea5e4d8d41aca51961ec758
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-01-17 10:19:38 -0300

    Update

--- IMPLEMENTACAO/Code/MHP7448_REF/02_gene_protein_trans.sh
+++ IMPLEMENTACAO/Code/MHP7448_REF/02_gene_protein_trans.sh
@@ -1,39 +1,61 @@
 #!/bin/bash
 
-# Arquivo FASTA de entrada
-file="/home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/strains/ATCC27716/Use/GCF_000367185.1/cds_from_genomic.fna"
+dir=/home/lgef
+#dir=/home/bryan
+
+mfc_list=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_list.txt
+for i in $(cat $mfc_list); do
+    input_fasta=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/input_proteins/proteins_id/MFC_${i}_proteins.fasta
+    id_map=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/input_proteins/dic_protein_gene/M_flocculare/${i}_dic.txt
+    output_fasta=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/input_proteins/genes_id/MFC_${i}_genes.fasta
+    
+    awk '
+        BEGIN {
+            while ((getline < "'"$id_map"'") > 0) {
+                map[$2] = $1
+            }
+            close("'"$id_map"'")
+        }
+        /^>/ {
+            split($1, id, ">")
+            if (id[2] in map) {
+                print ">" map[id[2]]
+            } else {
+                print $0
+            }
+            next
+        }
+        {
+            print $0
+        }' $input_fasta > $output_fasta
+done
 
 dir=/home/lgef
 #dir=/home/bryan
 
-for i in si; do  ; done
-
-input_fasta=/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Code/MHP7448_REF/MFC_ATCC27716_proteins.fasta
-id_map=/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Code/MHP7448_REF/Test.txt
-output_fasta=/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Code/MHP7448_REF/MFC_ATCC27716_proteins_trans.fasta
-
-# Criar o novo arquivo com IDs alterados
-awk '
-    BEGIN {
-        while ((getline < "'"$id_map"'") > 0) {
-            map[$2] = $1
+mfc_list=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_list.txt
+for i in $(cat $mfc_list); do
+    input_fasta=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/input_proteins/proteins_id/MFC_${i}_proteins.fasta
+    id_map=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/input_proteins/dic_protein_gene/M_flocculare/${i}_dic.txt
+    output_fasta=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/input_proteins/genes_id/MFC_${i}_genes.fasta
+    
+    awk '
+        BEGIN {
+            while ((getline < "'"$id_map"'") > 0) {
+                map[$2] = $1
+            }
+            close("'"$id_map"'")
         }
-        close("'"$id_map"'")
-    }
-    # Se for uma linha de cabeçalho FASTA (">"), substitua o ID se ele estiver no mapa
-    /^>/ {
-        split($1, id, ">")
-        if (id[2] in map) {
-            print ">" map[id[2]]
-        } else {
+        /^>/ {
+            split($1, id, ">")
+            if (id[2] in map) {
+                print ">" map[id[2]]
+            } else {
+                print $0
+            }
+            next
+        }
+        {
             print $0
-        }
-        next
-    }
-    # Caso contrário, imprima a sequência normalmente
-    {
-        print $0
-    }
-' "$input_fasta" > "$output_fasta"
-
-echo "IDs atualizados e salvos em $output_fasta"
+        }' $input_fasta > $output_fasta
+done
