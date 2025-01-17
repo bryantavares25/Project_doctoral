026ef123c7f2e7563480a41b3ae52b594bcc6610
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-01-17 09:56:18 -0300

    Update

--- IMPLEMENTACAO/Code/MHP7448_REF/02_gene_protein_dicio.sh
+++ IMPLEMENTACAO/Code/MHP7448_REF/02_gene_protein_dicio.sh
@@ -1,19 +1,39 @@
 #!/bin/bash
 
-# Arquivo FASTA de entrada
-file="/home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/strains/ATCC27716/Use/GCF_000367185.1/cds_from_genomic.fna"
+dir=/home/lgef
+#dir=/home/bryan
+
+mfc_list=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_list.txt
+for i in $(cat $mfc_list); do
+    reference=$(find "${dir}/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_flocculare/strains/${i}/Use/" -type f -path "*/G*.1/cds_from_genomic.fna")
+    dicionary=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/input_proteins/dic_protein_gene/M_flocculare/${i}_dic.txt
+    # Processar o arquivo para criar o dicionário
+    awk -F '[][]' '/^>/{ 
+        for (i=1; i<=NF; i++) { 
+            if ($i ~ /^locus_tag=/) locus_tag=substr($i, 11) 
+            if ($i ~ /^protein_id=/) protein_id=substr($i, 12) 
+        } 
+        if (locus_tag && protein_id) 
+            print locus_tag, protein_id
+    }' $reference > $dicionary
+done
+
+#!/bin/bash
 
 dir=/home/lgef
 #dir=/home/bryan
 
-for i in si; do  ; done
-
-# Processar o arquivo para criar o dicionário
-awk -F '[][]' '/^>/{ 
-    for (i=1; i<=NF; i++) { 
-        if ($i ~ /^locus_tag=/) locus_tag=substr($i, 11) 
-        if ($i ~ /^protein_id=/) protein_id=substr($i, 12) 
-    } 
-    if (locus_tag && protein_id) 
-        print locus_tag, protein_id
-}' $file > /home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Code/MHP7448_REF/Test.txt
+mhp_list=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mhp_list.txt
+for i in $(cat $mhp_list); do
+    reference=$(find "${dir}/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/strains/${i}/Use/" -type f -path "*/G*.1/cds_from_genomic.fna")
+    dicionary=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/input_proteins/dic_protein_gene/M_hyopneumoniae/${i}_dic.txt
+    # Processar o arquivo para criar o dicionário
+    awk -F '[][]' '/^>/{ 
+        for (i=1; i<=NF; i++) { 
+            if ($i ~ /^locus_tag=/) locus_tag=substr($i, 11) 
+            if ($i ~ /^protein_id=/) protein_id=substr($i, 12) 
+        } 
+        if (locus_tag && protein_id) 
+            print locus_tag, protein_id
+    }' $reference > $dicionary
+done
