cba9174a55397109f53169e32929536d4460b919
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-01-28 09:01:28 -0300

    Update

--- IMPLEMENTACAO/Code/03_GEN_CLUST/01_bed.sh
+++ IMPLEMENTACAO/Code/03_GEN_CLUST/01_bed.sh
@@ -3,8 +3,8 @@
 # START > > > OPERON VIEWER
 
 # ARCHIVE
-dir=/home/bryan
-#dir=/home/lgef
+#dir=/home/bryan
+dir=/home/lgef
 #org=$dir/Documents/Projects/Project_REGENERA/Project_VANESSA/Bioluminescentes
 
 # INPUT FILE
@@ -13,41 +13,80 @@ dir=/home/bryan
 #read -r line < $dir/Documents/Github/Project_regenera/VAN_projects/strain.txt
 #line=REG_001554_SEQ_240819201486
 #echo $line
-line=11
+#line=11
 
-input_gff=$(find "${dir}/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/${line}" -type f | grep -E ".*\.gff")
+for line in $(cat $mhp_list); do
+    input_gff=$(find "${dir}/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/${line}" -type f | grep -E ".*\.gff")
 
-pipe_bed=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/${line}/genomic.bed
-pipe_sor=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/${line}/genomic_sort.bed
-trun_bed=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/${line}/genomic_gene_coc.bed
+    pipe_bed=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/${line}/genomic.bed
+    pipe_sor=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/${line}/genomic_sort.bed
+    trun_bed=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_hyopneumoniae/${line}/genomic_gene_coc.bed
 
-# EXECUTION 
+    # EXECUTION 
 
-# .gff to .bed [Ajuste de formatação]
-awk 'BEGIN { OFS = "\t" } $2 == "RefSeq" && $3 != "region" { 
-        split($9, a, ";")
-        split(a[1], b, "=")
-        print $1, $4-1, $5, b[2], 0, $7
-    }' "$input_gff" > "$pipe_bed"
+    # .gff to .bed [Ajuste de formatação]
+    awk 'BEGIN { OFS = "\t" } $2 == "RefSeq" && $3 != "region" { 
+            split($9, a, ";")
+            split(a[1], b, "=")
+            print $1, $4-1, $5, b[2], 0, $7
+        }' "$input_gff" > "$pipe_bed"
 
-# Sort data [ordenação dos dados > Cromossomo:Inicio] 
-sort -k1,1 -k2,2n "$pipe_bed" > "$pipe_sor"
+    # Sort data [ordenação dos dados > Cromossomo:Inicio] 
+    sort -k1,1 -k2,2n "$pipe_bed" > "$pipe_sor"
 
-awk 'BEGIN { OFS = "\t" }
-{
-    if (NR == 1 || $6 == strand && $1 == chr) {
-        if (NR == 1) {
-            chr = $1; start = $2; end = $3; strand = $6; genes = $4
+    awk 'BEGIN { OFS = "\t" }
+    {
+        if (NR == 1 || $6 == strand && $1 == chr) {
+            if (NR == 1) {
+                chr = $1; start = $2; end = $3; strand = $6; genes = $4
+            } else {
+                end = $3; genes = genes "," $4
+            }
         } else {
-            end = $3; genes = genes "," $4
+            print chr, start, end, genes, strand
+            chr = $1; start = $2; end = $3; strand = $6; genes = $4
         }
-    } else {
-        print chr, start, end, genes, strand
-        chr = $1; start = $2; end = $3; strand = $6; genes = $4
     }
-}
-END {
-    print chr, start, end, genes, strand
-}' "$pipe_sor" > "$trun_bed"
+    END {
+        print chr, start, end, genes, strand
+    }' "$pipe_sor" > "$trun_bed"
+done
 
-# E N D > > > TO OPERON SELECTION
+for line in $(cat $mfc_list); do
+    input_gff=$(find "${dir}/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/${line}" -type f | grep -E ".*\.gff")
+
+    pipe_bed=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/${line}/genomic.bed
+    pipe_sor=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/${line}/genomic_sort.bed
+    trun_bed=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/${line}/genomic_gene_coc.bed
+
+    # EXECUTION 
+
+    # .gff to .bed [Ajuste de formatação]
+    awk 'BEGIN { OFS = "\t" } $2 == "RefSeq" && $3 != "region" { 
+            split($9, a, ";")
+            split(a[1], b, "=")
+            print $1, $4-1, $5, b[2], 0, $7
+        }' "$input_gff" > "$pipe_bed"
+
+    # Sort data [ordenação dos dados > Cromossomo:Inicio] 
+    sort -k1,1 -k2,2n "$pipe_bed" > "$pipe_sor"
+
+    awk 'BEGIN { OFS = "\t" }
+    {
+        if (NR == 1 || $6 == strand && $1 == chr) {
+            if (NR == 1) {
+                chr = $1; start = $2; end = $3; strand = $6; genes = $4
+            } else {
+                end = $3; genes = genes "," $4
+            }
+        } else {
+            print chr, start, end, genes, strand
+            chr = $1; start = $2; end = $3; strand = $6; genes = $4
+        }
+    }
+    END {
+        print chr, start, end, genes, strand
+    }' "$pipe_sor" > "$trun_bed"
+done
+
+    # E N D > > > TO OPERON SELECTION
