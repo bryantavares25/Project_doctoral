3156d90942d3187fa960ee2e24e57129a091f97a
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-01-29 23:10:10 -0300

    Update

--- IMPLEMENTACAO/Code/01_GEN_STRUC/06_new_positions_mhp.sh
+++ IMPLEMENTACAO/Code/01_GEN_STRUC/06_new_positions_mhp.sh
@@ -86,10 +86,3 @@ done
 # END > > > 
 # GENOMES TO GENE_CLUSTERS
 # A partir do arquivo gerado, será possível prosseguir para análises estruturais
-
-for file in $(cat $mfc_list); do
-    mfc_genes_location="$file_output_dir/genes_location.tsv"
-    file_output_dir="$output_dir/$file/Position_update"
-    mfc_genes_location_clean="$file_output_dir/genes_location_clean.tsv"
-    cp "$mfc_genes_location" "$mfc_genes_location_clean"
-done
