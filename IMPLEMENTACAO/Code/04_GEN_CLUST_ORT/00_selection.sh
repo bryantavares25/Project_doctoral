49f6a896150071689982f73c5759c2f3441c4b66
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-02-01 15:02:09 -0300

    Update 00_selection.sh

---
+++ IMPLEMENTACAO/Code/04_GEN_CLUST_ORT/00_selection.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# START > > > OPERON SELECTION
+
+OG=(OG0000003 OG0000011 OG0000013 OG0000055 OG0000072 OG0000078 OG0000100 OG0000145 OG0000169 OG0000215 OG0000248 OG0000278 OG0000306 OG0000359 OG0000427 OG0000458 OG0000472 OG0000497 OG0000523 OG0000532 OG0000537 OG0000538 OG0000553 OG0000599 OG0000608)
+
+# ARCHIVE
+dir=/home/bryan
+#dir=/home/lgef
+
+mhp_list=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mhp_list.txt
+
+# E X E C U T I O N
+
+for line in $(cat $mhp_list); do
+
+    echo $line
+
+    for gene in ${OG[@]}; do
+
+        echo $gene
+
+    done
+
+done
