e6bfce36ec76a77974e69644a6b9439637779190
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-02-02 21:13:56 -0300

    Update

--- IMPLEMENTACAO/Code/04_GEN_CLUST_ORT/00_selection.sh
+++ IMPLEMENTACAO/Code/04_GEN_CLUST_ORT/00_selection.sh
@@ -17,19 +17,30 @@ IMPLEMENTACAO/Gene_clusters/orthofinder/output_proteins/gene_id/Orthologues/MHP_
 #gene_id > Orthologues
 
 # Uma árvore 
-# Strain | OG | GENEID >>> pega o ID do gene >>> /home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/genomic_gene_coc.bed
+# Strain | OG | GENEID >>> pega o ID do gene >>> $dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/genomic_gene_coc.bed
+
+(a[i] ~ /^gene_biotype=/)
 
 # E X E C U T I O N
 
 for line in $(cat $mfc_list); do
-    echo "MHP_${line}_genes"
-
-    for gene in ${OG[@]}; do
-
-        echo $gene
-
+    for og in ${OG[@]}; do
+        echo "MFC_${line}_genes ${og}"
+        awk 'BEGIN {OFS = "\t"} if $1 ~ /MFC_${line}_genes/ $2 ~ /${og}_genes/ print $0' $mfc
     done
-
 done
+
+dir=/home/bryan
+OG=(OG0000003 OG0000011 OG0000013 OG0000055 OG0000072 OG0000078 OG0000100 OG0000145 OG0000169 OG0000215 OG0000248 OG0000278 OG0000306 OG0000359 OG0000427 OG0000458 OG0000472 OG0000497 OG0000523 OG0000532 OG0000537 OG0000538 OG0000553 OG0000599 OG0000608)
+mfc=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/orthofinder/output_proteins/gene_id/Orthologues/MHP_7448_genes.tsv
+mfc_list=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/mfc_list.txt
+
+while read -r line; do
+    for og in "${OG[@]}"; do
+        echo "MFC_${line}_genes ${og}"
+        awk -v line="MFC_${line}_genes" -v og="${og}_genes" 'BEGIN {OFS = "\t"} $1 ~ line && $2 ~ og {print $0}' "$mfc"
+    done
+done < "$mfc_list"
+
 
 # E N D
