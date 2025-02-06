6e652e899dd806c799cc1caeb1f1bd535ed32cf5
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-02-06 09:34:49 -0300

    Update 00_GS.sh

--- IMPLEMENTACAO/Code/05_GEN_STRUC/00_GS.sh
+++ IMPLEMENTACAO/Code/05_GEN_STRUC/00_GS.sh
@@ -10,12 +10,16 @@ dir=/home/bryan
 
 r=(OG0000011 OG0000072 OG0000078 OG0000100 OG0000145 OG0000169 OG0000215 OG0000248 OG0000278 OG0000306 OG0000359)
 
+/home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters_ort/gene_co_coc.tsv
 
 # EXECUTION
 
-#
+while IFS=$'\t' read -r c1 c2 c3 c4 c5 c6; do
 
-# 
+done
+# Recovery sequence
+
+# Construc GFF and Update
 
 #
 
