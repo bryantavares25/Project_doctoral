90c6d703c8565f0a0e8c11fbd1e8b968405e240e
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-02-07 11:33:24 -0300

    Update

--- IMPLEMENTACAO/Code/05_GEN_STRUC/00_GS.sh
+++ IMPLEMENTACAO/Code/05_GEN_STRUC/00_GS.sh
@@ -15,37 +15,55 @@ t=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters_ort/gene_c
 # EXECUTION
 
 while IFS=$'\t' read -r c1 c2 c3 c4 c5 c6 c7 c8; do
-    a=$((c5 - 250))
-    b=$((c6 + 250))
-    #echo $b
+    #for item in "${r[@]}"; do
+        #if [[ "$linha" == *"$item"* ]]; then
+    echo " - Iniciando linha"
+    if [[ " ${r[@]} " =~ " $c2 " ]]; then
+        echo "SoS"
+        a=$((c5 - 250))
+        b=$((c6 + 250))
+        
+        local=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/
+        mhp=M_hyopneumoniae/mult_align/seqs_to_align/$c1.fasta
+        mfc=M_flocculare/mult_align/seqs_to_align/$c1.fasta
 
-    local=$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/
-    mhp=M_hyopneumoniae/mult_align/seqs_to_align/$c1.fasta
-    mfc=M_flocculare/mult_align/seqs_to_align/$c1.fasta
+        ls $local$mhp 2>/dev/null && recip=$local$mhp || ls $local$mfc 2>/dev/null && recip=$local$mfc
+        
+        output_fasta="$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomic_structural/${c1}_${c2}_${c3}_${c4}.fasta"
+        output_gff="$dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomic_structural/${c1}_${c2}_${c3}_${c4}.fasta"
 
-    ls $local$mhp 2>/dev/null && recip=$local$mhp || ls $local$mfc 2>/dev/null && recip=$local$mfc
-    
-    output="/home/bryan/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomic_structural/${c1}_${c2}_${c3}_${c4}.fasta"
+        if [ "$c7" == "+" ]; then
+            # Recovery sequence
+            seqkit subseq -r "$a:$b" "$recip" -o $output_fasta
+        
+            # Construc GFF and Update
+            #c8="MHL_RS03735,MHL_RS02240,MHL_RS02245,MHL_RS02250,MHL_RS02255"
+            IFS=',' read -r -a l <<< "$c8"
+            for g in ${l[@]}; do
+                grep -E "RefSeq.*$g" $dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M*/${c1}/genomic.gff >> $dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M*/${c1}/genomic_target.gff
+            done
+            
+            tac $dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M*/${c1}/genomic_target.gff | \
+            awk '{temp=$4; $4=$5; $5=temp} 1' | \
+            awk 'NR==1 {delta = $4 - 250; $4 = 250; $5 -= delta} NR>1 {$4 -= delta; $5 -= delta} {print}' > $output_gff
+        else
+            # Recovery sequence
+            seqkit subseq -r "$a:$b" "$recip" | seqkit seq --reverse --complement > $output_fasta
+            
+            # Construc GFF and Update
+            IFS=',' read -r -a l <<< "$c8"
+            for g in ${l[@]}; do
+                grep -E "RefSeq.*$g" $dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M*/${c1}/genomic.gff >> $dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M*/${c1}/genomic_target.gff
+            done
 
-    if [ "$c7" == "+" ]; then
-        echo "+"
-        # Recovery sequence
-        seqkit subseq -r "$a:$b" "$recip" -o $output
-        # Construc GFF and Update
-        tac /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/01_genomic.gff | \
-        awk '{temp=$4; $4=$5; $5=temp} 1' | \
-        awk 'NR==1 {delta = $4 - 250; $4 = 250; $5 -= delta} NR>1 {$4 -= delta; $5 -= delta} {print}' > /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/02_genomic.gff
+            tac $dir/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M*/${c1}/genomic_target.gff | \
+            awk '{temp=$4; $4=$5; $5=temp} 1' | \
+            awk 'NR==1 {delta = $4 + 250; $4 = 250; $5 = -$5 +delta} NR>1 {$4 = -$4 + delta; $5 = -$5 + delta} {print}' > $output_gff
+        fi 
     else
-        echo "-"
-        # Recovery sequence
-        seqkit subseq -r "$a:$b" "$recip" | seqkit seq --reverse --complement > $output
-        # Construc GFF and Update
-        grep -F -f /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/ids.txt /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/genomic.gff > /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/01_genomic.gff
-        grep -E 'RefSeq.*MFC_RS00295' /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/genomic.gff >> /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/01_genomic.gff
-        tac /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/01_genomic.gff | \
-        awk '{temp=$4; $4=$5; $5=temp} 1' | \
-        awk 'NR==1 {delta = $4 + 250; $4 = 250; $5 = -$5 +delta} NR>1 {$4 = -$4 + delta; $5 = -$5 + delta} {print}' > /home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Gene_clusters/M_flocculare/ATCC27716/02_genomic.gff
-    fi 
+        echo "NoN"
+    fi
+    echo " - Finalizando linha"
 done < $t
 
 #seqid  source  type  start  end  score  strand  phase  attributes
