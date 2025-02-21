aadc3657e39c18fa0f754e28a59647b37e122bf0
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-02-21 11:06:19 -0300

    Update

--- IMPLEMENTACAO/Code/Base/05_GEN_STRUC/Teste_plot.py
+++ IMPLEMENTACAO/Code/Base/05_GEN_STRUC/Teste_plot.py
@@ -1,38 +1,35 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from upsetplot import UpSet
 
 # Configurar estilo bonito
-sns.set(style="whitegrid", context="talk", font="sans-serif")
+#sns.set(style="whitegrid", context="talk", font="sans-serif")
 
 # Criando um DataFrame binário (1 = presente, 0 = ausente)
 df = pd.DataFrame({
-    "A": [1, 1, 0, 1, 0, 1, 1, 0, 1, 1],
-    "B": [0, 1, 1, 1, 1, 0, 0, 1, 0, 1],
-    "C": [1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-    "D": [1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-    "E": [1, 0, 1, 1, 0, 1, 0, 1, 0, 1],
-    "F": [1, 0, 1, 1, 0, 1, 0, 1, 0, 1]
+    "M ATCC27716": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0],
+    "M MF11": [1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,0],
+    "M MF18": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,0],
+    "M MF22": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,0],
+    "M MF29": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,0],
+    "M MF30": [0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,1,0,1,0],
+    "M MF33": [1,1,1,0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0],
+    "M 42": [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,1,0]
 })
 
-# Contando a frequência de cada combinação de conjuntos
-upset_data = df.groupby(list(df.columns)).size()
+print(df)
+#upset_data = df.columns() #.groupby(list(df.columns)).size()
+upset_data = df.groupby(list(df.columns)).size().reset_index(name="count")
+# Exibir corretamente os resultados
+print(upset_data)
+
+#upset_data = df.groupby(df.columns)#.size() #.groupby(list(df.columns)).size()
 
 # Criando o gráfico UpSet
 fig, ax = plt.subplots(figsize=(8, 6), facecolor="white")
 
 upset = UpSet(upset_data, sort_categories_by="-input")
-upset.style_subsets(min_subset_size=1, edgecolor="white", linewidth=0.1)
+#upset.style_subsets(min_subset_size=1, edgecolor="white", linewidth=0.1)
 
-# Removendo as linhas de conexão
-for ax in plt.gcf().axes:  
-    for line in ax.get_lines():  
-        line.remove()  # Remove as linhas do gráfico
 upset.plot(fig=fig)
-
-# Ajustando título
-plt.suptitle("UpSet sem Linhas de Conexão", fontsize=16, fontweight="bold")
-
-# Exibir o gráfico
 plt.show()
