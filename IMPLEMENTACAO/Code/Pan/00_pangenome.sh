893b7b630220090e0a8e4e1031d02f2a4629ad75
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-03-08 21:56:50 -0300

    Update

--- IMPLEMENTACAO/Code/Pan/00_pangenome.sh
+++ IMPLEMENTACAO/Code/Pan/00_pangenome.sh
@@ -3,17 +3,26 @@
 #pangenome worflow
 
 # Download NCBI genomes
+#   Total
 ncbi-genome-download bacteria -g "Mesomycoplasma hyopneumoniae" --metadata MHP-NCBI-METADATA.txt -F all
+#   Util
+ncbi-genome-download bacteria -g "Mesomycoplasma hyopneumoniae" --metadata MHP-NCBI-METADATA.txt
+
 ncbi-genome-download bacteria -g "Mesomycoplasma flocculare" --metadata MFC-NCBI-METADATA.txt -F all
 
 #
 # INPUT > MHP-NCBI-METADATA.txt
-anvi-script-process-genbank-metadata -m MHP-NCBI-METADATA.txt
-# OUTPUT > fasta-input.txt
-# fasta-input | functions.txt | external-gene-calls.txt | contigs-fasta.txt
-'''
+anvi-script-process-genbank-metadata -m MHP-NCBI-METADATA.txt # -o output_folder
+# OUTPUT > fasta-input.txt | functions.txt | external-gene-calls.txt | contigs-fasta.fa
+# fasta-input > name	path	external_gene_calls	gene_functional_annotation
 
-'''
+# Ajuste de NCBI_PGAP para prodigal para não ter erro
+find -type f -exec sed -i 's/NCBI_PGAP/prodigal/g' {} +
+
+anvi-run-workflow -w pangenomics --get-default-config PAN-CONFIG.json
+
+
+anvi-run-workflow -w pangenomics -c PAN-CONFIG.json
 
 
 # Banco de dados para anotação funcional
