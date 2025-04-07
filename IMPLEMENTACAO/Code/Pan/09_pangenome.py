2f9e86aed31dc243e1f16d90feec87f1e45b796d
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-04-07 11:05:27 -0300

    Update 09_pangenome.py

--- IMPLEMENTACAO/Code/Pan/09_pangenome.py
+++ IMPLEMENTACAO/Code/Pan/09_pangenome.py
@@ -67,6 +67,7 @@ goeaobj.prt_tsv(f2, goea_results2)
 f2.close()
 '''
 
+'''
 from goatools.obo_parser import GODag
 from goatools.goea.go_enrichment_ns import GOEnrichmentStudy
 from goatools.associations import read_gaf
@@ -97,4 +98,66 @@ goeaobj = GOEnrichmentStudy(
 results = goeaobj.run_study(study_ids)
 
 # 6. Salvar resultados
-goeaobj.wr_xlsx("resultado_enriquecimento.xlsx", results)
+goeaobj.wr_xlsx("/home/lgef/InterTests/resultado_enriquecimento.xlsx", results)
+'''
+import csv
+from collections import defaultdict
+from goatools.obo_parser import GODag
+from goatools.goea.go_enrichment_ns import GOEnrichmentStudy
+
+def carregar_go_termos(arquivo):
+    """Carrega dados de arquivos TSV no formato gene-GO_terms."""
+    gene2go = defaultdict(set)
+    with open(arquivo, "r") as f:
+        reader = csv.reader(f, delimiter="\t")
+        next(reader)  # Pular cabeçalho
+        for row in reader:
+            if len(row) < 2:
+                continue
+            gene_id, go_terms = row[0], row[1]
+            if go_terms:  # Verificar se há termos GO
+                gene2go[gene_id].update(go_terms.split(","))
+    return gene2go
+
+# Carregar os dois grupos
+grupo1 = carregar_go_termos("/home/lgef/InterTests/mhp_output.tsv")
+grupo2 = carregar_go_termos("/home/lgef/InterTests/mfc_output.tsv")
+
+# Criar listas de genes
+genes_grupo1 = set(grupo1.keys())
+genes_grupo2 = set(grupo2.keys())
+genes_background = genes_grupo1.union(genes_grupo2)
+
+# Unir anotações GO
+gene2go = defaultdict(set)
+gene2go.update(grupo1)
+for gene, gos in grupo2.items():
+    gene2go[gene].update(gos)
+
+# Carregar ontologia GO
+godag = GODag("/home/lgef/go-basic.obo")
+
+# Criar objeto de enriquecimento
+goeaobj = GOEnrichmentStudy(
+    genes_background,
+    gene2go,
+    godag,
+    propagate_counts=True,
+    methods=["fdr_bh"],
+    alpha=0.05
+)
+
+# Rodar análises
+goea_results1 = goeaobj.run_study(genes_grupo1)
+goea_results2 = goeaobj.run_study(genes_grupo2)
+
+# Imprimir resultados - FORMA CORRETA
+print("\n=== Resultados para Grupo 1 ===")
+#goeaobj.print_results(goea_results1)  # Método correto para imprimir
+
+print("\n=== Resultados para Grupo 2 ===")
+#goeaobj.print_results(goea_results2)
+
+# Salvar resultados em TSV
+goeaobj.wr_tsv("/home/lgef/InterTests/mhp_go_enrichment_grupo1.tsv", goea_results1)
+goeaobj.wr_tsv("/home/lgef/InterTests/mfc_go_enrichment_grupo2.tsv", goea_results2)
