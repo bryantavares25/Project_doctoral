8b4df40d18295e385908413298fa20fdef0e7b15
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-04-08 10:38:36 -0300

    Update 11_pangenome_interpro.sh

--- IMPLEMENTACAO/Code/Pan/11_pangenome_interpro.sh
+++ IMPLEMENTACAO/Code/Pan/11_pangenome_interpro.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 
+dir=/home/lgef/
+mhp=Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae
+
+#/home/lgef/Documentos/GitHub/Project_doctoral/IMPLEMENTACAO/Genomes/M_hyopneumoniae/strains/11/Use/GCF_002193015.1/protein.faa
+
+#interproscan
+
