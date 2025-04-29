5b868b6d5cf14bb80f5a3a74a3c0b0a2469a460c
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-04-29 10:59:12 -0300

    Update 16_Functional_enrichment_fractions.py

--- IMPLEMENTACAO/Code/Pan/16_Functional_enrichment_fractions.py
+++ IMPLEMENTACAO/Code/Pan/16_Functional_enrichment_fractions.py
@@ -1,3 +1,6 @@
+
+# CHATGPT #
+
 from goatools.obo_parser import GODag
 from goatools.associations import read_gaf
 from goatools.go_enrichment import GOEnrichmentStudy
@@ -33,3 +36,34 @@ results = goea_obj.run_study(study_genes)
 
 # Mostrar resultados significativos
 goea_obj.print_results(results, min_ratio=0.05, pval=0.05)
+
+# CHATGPT #
+
+# DEEPSEEK #
+from goatools import obo_parser
+from goatools.go_enrichment import GOEnrichmentStudy
+
+# Carregar a ontologia GO
+go_obo = "go-basic.obo"  # Arquivo OBO da GO
+go = obo_parser.GODag(go_obo)
+
+# Carregar anotações (gene-GO)
+gene2go = "gene2go.txt"  # Formato: gene \t GO_ID
+associations = {}
+with open(gene2go) as f:
+    for line in f:
+        gene, go_id = line.strip().split('\t')
+        associations.setdefault(gene, set()).add(go_id)
+
+# Definir genes de interesse e população de referência
+study_genes = ["gene1", "gene2", "gene3"]  # Exemplo
+population_genes = list(associations.keys())  # Todos os genes anotados
+
+# Executar análise de enriquecimento
+methods = ["bonferroni", "fdr"]  # Correções para múltiplos testes
+g = GOEnrichmentStudy(population_genes, associations, go, methods=methods)
+result = g.run_study(study_genes)
+
+# Salvar resultados
+g.write_summary("enriquecimento_go.txt", result)
+# DEEPSEEK #
