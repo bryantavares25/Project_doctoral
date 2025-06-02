b1431e31075e6cf1ba45d071e0a45e17a634e96e
Bryan Tavares <106768423+bryantavares25@users.noreply.github.com>
2025-06-02 10:53:32 -0300

    Update 30_classification_fraction.py

--- IMPLEMENTACAO/Code/Pan/30_classification_fraction.py
+++ IMPLEMENTACAO/Code/Pan/30_classification_fraction.py
@@ -1,8 +1,32 @@
 #!python3
 
+# Importação de bibliotecas
 import pandas as pd
 
-file="/home/lgef/Downloads/01_MFC_fractions.csv"
-df = pd.read_csv(file, delimiter=';')
+# Definição
+def classify_type(row):
+    functional = row["Functional homogeneity index"]
+    geometric = row["Geometric homogeneity index"]
+    
+    if functional < 1 and geometric == 1:
+        return "Functional"
+    elif functional == 1 and geometric < 1:
+        return "Geometric"
+    elif functional < 1 and geometric < 1:
+        return "Functioal and Geometric"
+    else:
+        return "" # ou outro valor padrão
 
+# Importação de dados
+file="/home/lgef/Downloads/01_MX_fractions.csv"
+df = pd.read_csv(file, delimiter=',')
+
+# Aplicar a função e criar a coluna Type
+df["Type"] = df.apply(classify_type, axis=1)
+
+# Salvar o DataFrame modificado (opcional)
+output_file = "/home/lgef/Downloads/01_MX_fractions_classified.csv"
+df.to_csv(output_file, index=False)
+
+# Mostrar o DataFrame
 print(df)
