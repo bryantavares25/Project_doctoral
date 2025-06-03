97a4a2b8f08db691d0370ca4356cc95788c9a7c6
Bryan Tavares <bryan.ar.tavares@gmail.com>
2025-06-03 20:51:08 -0300

    Update 31_data _presentation.py

---
+++ IMPLEMENTACAO/Code/Pan/31_data _presentation.py
@@ -0,0 +1,12 @@
+#!python3
+
+import pandas as pd
+
+mhp="/home/bryantavares/Documents/GitHub/Project_doctoral/Data/01_MHP_fractions_classified.csv"
+mfc="/home/bryantavares/Documents/GitHub/Project_doctoral/Data/01_MFC_fractions_classified.csv"
+mx="/home/bryantavares/Documents/GitHub/Project_doctoral/Data/01_MX_fractions_classified.csv"
+
+df=pd.read_csv(mhp)
+print(df.)
+
+# DRAFT
